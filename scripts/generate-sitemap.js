ed62078b89db90b962edbf55649414a5dd73ce02
Serhii Cherneha <chernegasergiy3@gmail.com>
2025-10-02 22:40:40 +0300

    fix(sitemap): Correctly place sitemap tags

--- scripts/generate-sitemap.js
+++ scripts/generate-sitemap.js
@@ -12,16 +12,17 @@ function generateSitemap() {
   xml += `  <url>\n`;
   xml += `    <loc>${BASE_URL}</loc>\n`;
 
+  xml += `    <lastmod>${new Date().toISOString().split('T')[0]}</lastmod>\n`;
+  xml += `    <changefreq>monthly</changefreq>\n`;
+  xml += `    <priority>1.0</priority>\n`;
+
   for (const lang of availableLanguages) {
     xml += `    <xhtml:link rel="alternate" hreflang="${lang}" href="${BASE_URL}" />\n`;
   }
 
   xml += `    <xhtml:link rel="alternate" hreflang="x-default" href="${BASE_URL}" />\n`;
-  xml += `    <lastmod>${new Date().toISOString().split('T')[0]}</lastmod>\n`;
-  xml += `    <changefreq>monthly</changefreq>\n`;
-  xml += `    <priority>1.0</priority>\n`;
-  xml += `  </url>\n`;
 
+  xml += `  </url>\n`;
   xml += `</urlset>\n`;
   return xml;
 }
